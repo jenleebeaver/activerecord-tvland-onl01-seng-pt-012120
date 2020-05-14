bc107a7c73d14aecd83b84337c83c5614681ba2e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-14 19:25:47 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
-  attr_accessor :catchphrase
 
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
